54f5ca455f868c969bc76867c5f8cd8d486b6b21
Usuario <Usuario@SPLab1-12>
2024-03-15 08:36:24 -0600

    Primra carga:

---
+++ src/main/java/ucr/ac/cr/progamacionll/Progamacionll.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.ac.cr.progamacionll;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Progamacionll {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
